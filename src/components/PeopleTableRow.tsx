14d7d084b6ee97d890236284443156c33d0b3cc3
Mateusz <m.wesolowski.92@gmail.com>
2025-02-17 10:41:40 +0100

    implement PeopleTableRow with clickable rows and navigation

---
+++ src/components/PeopleTableRow.tsx
@@ -0,0 +1,50 @@
+import { useNavigate } from 'react-router-dom';
+import { PeopleTableRowProps } from '../types/types';
+
+export const PeopleTableRow = ({
+  data: {
+    name,
+    height,
+    mass,
+    birth_year,
+    eye_color,
+    hair_color,
+    skin_color,
+    url,
+  },
+}: PeopleTableRowProps) => {
+  const navigate = useNavigate();
+
+  const handleOnClick = (url: string) => {
+    const parts = url.split('/');
+    const id = parts[parts.length - 2];
+    navigate(`/people/${id}`);
+  };
+
+  return (
+    <tr
+      className="text-center hover:bg-gray-100 cursor-pointer"
+      onClick={() => handleOnClick(url)}
+    >
+      <td className="border border-gray-400 p-2 whitespace-nowrap">{name}</td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {height} cm
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {mass} kg
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {birth_year}
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {eye_color}
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {hair_color}
+      </td>
+      <td className="border border-gray-400 p-2 whitespace-nowrap">
+        {skin_color}
+      </td>
+    </tr>
+  );
+};
