2d79950bd83fcd90ebf80eb576f891caf4ac133e
Mateusz <m.wesolowski.92@gmail.com>
2025-02-17 14:00:55 +0100

    remove useEffect to keep data in store

--- src/hooks/useFetchPeople.ts
+++ src/hooks/useFetchPeople.ts
@@ -1,10 +1,7 @@
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { fetchPeople } from '../api/peopleApi';
-import { usePeopleStore } from '../store/usePeopleStore';
-import { useEffect } from 'react';
 
 export const useFetchPeople = (search: string, page: number) => {
-  const { setCount } = usePeopleStore();
   const {
     data: people,
     isLoading,
@@ -16,11 +13,6 @@ export const useFetchPeople = (search: string, page: number) => {
     retry: 1,
     staleTime: 60_000,
   });
-
-  useEffect(() => {
-    if (!people) return;
-    setCount(people?.count);
-  }, [people, setCount]);
 
   return { people, isLoading, isError };
 };
