7b62b0c6cbfd8e444bcbe82a54f362c89d2655be
Mateusz <m.wesolowski.92@gmail.com>
2025-02-17 10:34:24 +0100

    setup routing and react-query integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+import App from './App.tsx';
+import { SingleViewPage } from './pages/SingleViewPage';
+import { NotFoundPage } from './pages/NotFoundPage.jsx';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    element: <Navigate to="/people" />,
+    path: '/',
+    errorElement: <NotFoundPage />,
+  },
+  {
+    element: <App />,
+    path: '/people',
+  },
+  {
+    element: <SingleViewPage />,
+    path: '/people/:id',
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+  // </StrictMode>,
+);
