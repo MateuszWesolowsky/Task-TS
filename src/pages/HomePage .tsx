efea53bee9ba3501244068af3ad6a0c84351347c
Mateusz <m.wesolowski.92@gmail.com>
2025-02-17 14:02:01 +0100

    remove logic to fetch data

--- src/pages/HomePage .tsx
+++ src/pages/HomePage .tsx
@@ -1,16 +1,8 @@
 import { FilterInput } from '../components/FilterInput';
 import { Header } from '../components/Header';
-import { Pagination } from '../components/Pagination';
 import { PeopleTableList } from '../components/PeopleTableList';
-import { usePeopleStore } from '../store/usePeopleStore';
-import { useDebounce } from '../hooks/useDebounce';
-import { useFetchPeople } from '../hooks/useFetchPeople';
 
 export const HomePage = () => {
-  const { search, page } = usePeopleStore();
-  const debouncedSearch = useDebounce(search, 500);
-  const { people, isLoading, isError } = useFetchPeople(debouncedSearch, page);
-
   return (
     <div className="p-2 sm:p-4 mt-8">
       <Header
@@ -20,12 +12,7 @@ export const HomePage = () => {
         }
       />
       <FilterInput />
-      <PeopleTableList
-        people={people}
-        isLoading={isLoading}
-        isError={isError}
-      />
-      <Pagination />
+      <PeopleTableList />
     </div>
   );
 };
