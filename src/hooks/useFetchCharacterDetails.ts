f52ee9af71de616c013ccce3c0fe72b056bb3c4e
Mateusz <m.wesolowski.92@gmail.com>
2025-02-17 13:24:14 +0100

    fixed error with keepPreviousData

--- src/hooks/useFetchCharacterDetails.ts
+++ src/hooks/useFetchCharacterDetails.ts
@@ -1,11 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { fetchCharacterDetails } from '../api/deatilsApi';
 
 export const useFetchCharacterDetails = (id?: string) => {
   return useQuery({
     queryKey: ['character', id],
     queryFn: () => fetchCharacterDetails(id!),
-    keepPreviousData: true,
+    placeholderData: keepPreviousData,
     enabled: !!id,
     retry: 1,
     staleTime: 60_000,
