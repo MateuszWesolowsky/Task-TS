f9838562cd29b6b879a10f52c6c2ea6bad7601a7
Mateusz <m.wesolowski.92@gmail.com>
2025-02-24 10:07:20 +0100

    Improved header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 }
 
 export const Header = ({ title, description }: HeaderProps) => (
-  <header className="bg-blue-600 text-white py-6 px-4 rounded-md shadow-md mb-4 text-center">
+  <header className="bg-gradient-to-r from-blue-400 to-indigo-500 mt-8 text-white py-6 px-4 rounded-md shadow-md mb-4 text-center">
     <h1 className="text-3xl font-bold">{title}</h1>
     <p className="mt-2 text-lg">{description}</p>
   </header>
